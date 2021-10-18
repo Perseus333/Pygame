87a4bb03a2749eead1e0eca288f0b6133d7f93e0
Perseus-Lynx <perseusmith73@gmail.com>
2021-10-18 14:15:53 +0200

    Update rainbow-paint.py

--- RainbowPaint/rainbow-paint.py
+++ RainbowPaint/rainbow-paint.py
@@ -1,21 +1,23 @@
 import pygame
 import random
 
+
 def main():
+    
     white = (255, 255, 255)
 
     pygame.init()
     positions = [(0, 0)]
-    colors = [(0, 0, 0)]
-    pressed_keys = pygame.key.get_pressed()
-
-
+    colors = [(0, 0, 0)
+              
+              
     while True:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     quit()
+              
         screen = pygame.display.set_mode((600, 400))
         pygame.display.set_caption("Paint")
         screen.fill(white)
@@ -25,6 +27,7 @@ def main():
             random_color = (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
             positions.append(mouse_pos)
             colors.append(random_color)
+              
         for actual_pos in positions:
             pygame.draw.circle(screen, colors[positions.index(actual_pos)], actual_pos, 20)
         pygame.display.update()
