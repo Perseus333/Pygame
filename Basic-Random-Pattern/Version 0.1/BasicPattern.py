354c749dcaed831ef064afc63618bf9e297d1f5a
Perseus-Lynx <perseusmith73@gmail.com>
2021-10-19 19:43:47 +0200

    Update BasicPattern.py

--- Basic-Random-Pattern/Version 0.1/BasicPattern.py
+++ Basic-Random-Pattern/Version 0.1/BasicPattern.py
@@ -1,1 +1,48 @@
+import pygame
+import random
 
+
+def main():
+
+    WIDTH = 3000
+    HEIGHT = 1000
+
+    WHITE = (255, 255, 255)
+
+    starting_point = (WIDTH // 2, HEIGHT // 2)
+    positions = [starting_point]
+
+    separation = int(input('Type the separation between dots(int):'))
+
+    pygame.init()
+
+    while True:
+
+        # To close the game when ESC is pressed
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    quit()
+
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Paint")
+        screen.fill(WHITE)
+        
+        # The separation is the pixels allowed for the dot to move
+        rand_x = random.randint(separation*-1, separation)
+        rand_y = random.randint(separation*-1, separation)
+
+        # To maintain the trail of previous dots
+        last_pos = positions[len(positions) - 1]
+        next_position = (last_pos[0]+rand_x, last_pos[1]+rand_y)
+        positions.append(next_position)
+
+        for actual_pos in positions:
+            pygame.draw.circle(screen, (0, 0, 0), actual_pos, 1)
+
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
